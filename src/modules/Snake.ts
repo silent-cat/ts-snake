d7e6368bfa62285070ee21f42dcc3164745b1a9a
qizhixin <13284167983@163.com>
2021-01-29 15:07:28 +0800

    初步完成Snake类

---
+++ src/modules/Snake.ts
@@ -0,0 +1,33 @@
+class Snake {
+  head: HTMLElement //表示蛇头的的元素
+  bodies: HTMLCollection //表示蛇身
+  snake: HTMLElement
+  constructor() {
+    this.snake = document.getElementById('snake')!
+    this.head = document.querySelector('#snake>div') as HTMLElement
+    this.bodies = this.snake.getElementsByTagName('div')
+  }
+
+  //   获取蛇的坐标
+  get X() {
+    return this.head.offsetLeft
+  }
+
+  get Y() {
+    return this.head.offsetTop
+  }
+  //   设置蛇头的坐标
+  setX(val: number) {
+    this.head.style.left = val + 'px'
+  }
+  setY(val: number) {
+    this.head.style.top = val + 'px'
+  }
+
+  //   增加身体
+  addBody() {
+    this.snake.insertAdjacentHTML('beforeend', '<div></div>')
+  }
+}
+
+export default Snake
